05a4d91b06c47246b3a60dff1d82e3d083a4741f
Suzy-Sama <salmaelouafi002@gmail.com>
2023-03-16 20:05:39 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of char : %lu byte(s)\n", (unisgend long)sizeof(a));
+printf("size of char : %lu byte(s)\n", (unisgend long)sizeof(b));
+printf("size of char : %lu byte(s)\n", (unisgend long)sizeof(c));
+printf("size of char : %lu byte(s)\n", (unisgend long)sizeof(d));
+printf("size of char : %lu byte(s)\n", (unisgend long)sizeof(f));
+return (0);
+}
