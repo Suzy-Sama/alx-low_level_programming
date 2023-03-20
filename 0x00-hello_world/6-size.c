6293f0b39115368fd3601226c37b14db2018cc26
Suzy-Sama <salmaelouafi002@gmail.com>
2023-03-20 10:06:58 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,6 @@ printf("size of int : %lu byte(s)\n", sizeof(int));
 printf("size of long int : %lu byte(s)\n", sizeof(long int));
 printf("size of long long int : %lu byte(s)\n", sizeof(long long int));
 printf("size of float : %lu byte(s)\n", sizeof(float));
+
 return (0);
 }
