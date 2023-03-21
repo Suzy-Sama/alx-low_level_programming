5695c5e99e351fd9854b77d670f8611f3c405c98
Suzy-Sama <salmaelouafi002@gmail.com>
2023-03-20 19:31:19 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,26 +10,27 @@
 
 int main(void)
 {
-	int n, m;
+	int d, p;
 
-	for (n = 0; n < 100; n++)
+	for (d = '0'; d < '9'; d++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (p = d + 1; p <= '9'; p++)
 		{
-			if (m > n)
+			if (p != d)
 			{
-		putchar((n);
-		putchar((m);
-		if (n != 56 || m != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		}
-		}
+				putchar(d);
+				putchar(p);
 
+				if (d == '8' && p == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 
 	return (0);
 }
+
