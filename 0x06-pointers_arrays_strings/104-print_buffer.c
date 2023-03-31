31ffdc003a3257c1271cdca447dbc0c82701ed9d
Suzy-Sama <salmaelouafi002@gmail.com>
2023-03-30 19:59:49 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,53 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * print_buffer
- * Return : void
+ * print_buffer - print a buffer 10 bytes at a time, displaying the line
+ * number in hex and byte pairs in hex
+ *
+ * @b: string to print from
+ * @size: size of buffer
+ *
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
-int o, j, i;
-o=0;
+	int i, counter = 0, size2 = size;
+	char *ptr = b;
 
-if (size <= 0)
-{
-printf("\n");
-return;
-}
-while (o < size)
-{
-j = size -o < 10 ? size - o : 10;
-printf("%08x: ", o);
-for (i = 0; i < 10; i++)
-{
-if (i < j)
-printf("%02x:", *(b + o i));
-else
-printf("  ");
-if (i % 2)
-{
-printf("  ");
-}
-}
-for (i = 0; i < j; i++)
-{
-int c = *(b + o i);
-if (c < 32 || c > 132)
-{
-c = '.';
-}
-printf("%c", c);
-}
-printf("\n");
-o += 10;
-}
+	if (size == 0)
+		printf("\n");
+	while (size > 0)
+	{
+		printf("%08x:", counter);
+		i = 0;
+		while (size > 0 && i < 10)
+		{
+			if (!(i % 2))
+				printf(" ");
+			printf("%02x", *ptr++);
+			i++;
+			size--;
+			counter++;
+		}
+		while (counter % 10 != 0)
+		{
+			if (!(counter % 2))
+				printf(" ");
+			printf("  ");
+			counter++;
+		}
+		printf(" ");
+		i = 0;
+		while (size2 > 0 && i < 10)
+		{
+			if (*b > 31 && *b != 127)
+				printf("%c", *b);
+			else
+				printf(".");
+			size2--;
+			i++;
+			b++;
+		}
+		printf("\n");
+	}
 }
